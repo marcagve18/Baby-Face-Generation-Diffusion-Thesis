de3fe6da03c6904ef94d884a8191bdfeb63c0eb6
Marc Aguilar <marcaguilar.1803@gmail.com>
2024-02-23 16:34:35 +0100

    Refiner correctly working

--- scripts/inference/refiner/refine.py
+++ scripts/inference/refiner/refine.py
@@ -6,6 +6,8 @@ from diffusers import (
     ControlNetModel,
     AutoencoderKL
 )
+from datetime import datetime
+import os
 
 def load_image(path : str):
     with Image.open(path) as img:
@@ -25,7 +27,7 @@ def predict(args):
             "image": image,
             "control_image": image,
             "strength": args.strength,
-            "controlnet_conditioning_scale": 0.75,
+            "controlnet_conditioning_scale": 0.9,
             "negative_prompt": negative_prompt,
             "guidance_scale": 7,
             "generator": generator,
@@ -34,27 +36,33 @@ def predict(args):
         }
 
     controlnet = ControlNetModel.from_pretrained(
-            "lllyasviel/control_v11f1e_sd15_tile",
-            torch_dtype=torch.float16
-        )
+        "lllyasviel/control_v11f1e_sd15_tile",
+        torch_dtype=torch.float16
+    )
     
     vae = AutoencoderKL.from_pretrained(
         "stabilityai/sd-vae-ft-ema",
         torch_dtype=torch.float16
     )
 
+    base_model = "SG161222/Realistic_Vision_V5.1_noVAE"
     pipeline = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
-            "SG161222/Realistic_Vision_V5.1_noVAE",
-            vae=vae,
-            torch_dtype=torch.float16,
-            controlnet=controlnet
-        ).to("cuda")
+        base_model,
+        vae=vae,
+        torch_dtype=torch.float16,
+        controlnet=controlnet
+    ).to("cuda")
     
     pipeline.safety_checker = None
     pipeline.enable_xformers_memory_efficient_attention()
 
     output = pipeline(**model_args).images[0]
-    print(output)
+
+    current_datetime = datetime.now()
+    # Format the date and time as HH:MM:SS DD/MM/YY
+    filename = f"img_{current_datetime}.png"
+
+    output.save(f"{os.environ.get('HOME_PATH')}/TFG/Baby-Face-Generation-Diffusion-Thesis/output/images/tests/{filename}")
 
 
 def parse_args():
@@ -81,7 +89,7 @@ def parse_args():
     parser.add_argument(
         '--strength', 
         type=int, 
-        default=0.25, 
+        default=0.12, 
         help='Refinement strength'
     )
     return parser.parse_args()
